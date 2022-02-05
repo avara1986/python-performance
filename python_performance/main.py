ad87cdfbdc751af7c4402cf15b29d78d97e37d0f
avara1986 <a.vara.1986@gmail.com>
2022-02-05 20:39:09 +0100

    feat: func_log_n

--- python_performance/main.py
+++ python_performance/main.py
@@ -48,6 +48,6 @@ def func_0c(mi_lista):
 def my_function():
     # func_0c([i for i in range(500)])
     # func_0n([i for i in range(500)])
-    # func_log_n([i for i in range(500)])
+    func_log_n([i for i in range(500)])
     # func_n_2([i for i in range(500)])
-    func_2_n(33)
+    # func_2_n(33)
